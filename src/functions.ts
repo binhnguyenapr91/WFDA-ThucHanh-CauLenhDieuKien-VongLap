ea8008d0dd36358e5b99f36f7ff69ace189f5639
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-02 09:02:04 +0700

    finished

--- src/functions.ts
+++ src/functions.ts
@@ -1,25 +1,31 @@
 import './scss/styles.scss';
 
-function square(num: number): number {
-  return num * num;
+function addNumber (num:number): number{
+  return num*num;
 }
 
-console.log(square(5));
-// Output: 25
+console.log(addNumber(5));
 
-// Function expression
-const squareFE = function (num: number): number {
-  return num * num;
+const addNumber1 = function (number: number): number{
+  return number+number;
 }
 
-// Higher-order function
-function add(a: number): Function {
-  return function(b: number): number {
-    return a + b;
-  }
+console.log(addNumber1(7));
+
+//Higher order function
+let wrapperFunction = function (a:number) : Function{
+    return function (b: number) : number{
+      return a+b;
+    }
 }
 
-const addWith5 = add(5);
+const addWith5 = wrapperFunction(5);
 console.log(addWith5(3));
-console.log(addWith5(15));
+//Arrow function
 
+let getResult = (number1 : number, number2 : number):number => {
+  return number1+number2;
+}
+
+
+console.log(getResult(1,2));
